e149408b50103b0edb2b4c006412abbf43f29567
skalwar1796 <sanket@yantraai.tech>
2019-12-19 16:20:19 +0530

    optimization required

--- k-means.py
+++ k-means.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-K = 3 #number of clusters
+plt.ion()
+K = 2 #number of clusters
+epoches = 100 #number of iteration
+eps = 1e-6
 data = pd.read_csv('OldFaithful.csv')
 print('data',data.keys().shape[0]) #number of attributes of dataset.
 x = np.expand_dims(data['TimeEruption'].to_numpy(),axis=1)
@@ -15,15 +17,19 @@ for i in range(dataset.shape[1]):
     
 mean = np.random.rand(K,dataset.shape[1])
 score_map = np.zeros((dataset.shape[0],K))
-print('Number of clusters:-',K)
-print('dataset',dataset.shape)
-print('Mean shape',mean.shape)
-print('Score map',score_map.shape)
 
-for n in range(dataset.shape[0]):
-    list_temp = []
-    for k in range(K):
-        list_temp.append(np.sum(np.power(dataset[n,:]-mean[k,:],2)))
-    score_map[n,np.array(list_temp).argmax()] = 1
+for epoch in range(epoches):
+    score_map = np.zeros((dataset.shape[0],K))
+    for i in range(K):
+        score_map[:,i] = np.sum(np.power(dataset-mean[i,:],2),axis=1)
+    k_index = np.argmax(score_map,axis=1)
 
-print('score_map',score_map)
+    for i in range(K):
+        mean[i,:] = np.sum(dataset[np.where(k_index==i)[0],:],axis=0)/(np.where(k_index==i)[0].shape[0]+eps)
+
+    for i in range(K):
+        plt.scatter(dataset[np.where(k_index==i)[0],0],dataset[np.where(k_index==i)[0],1])
+        plt.scatter(mean[:,0],mean[:,1],marker='+')
+        plt.savefig('cluster.png')
+        plt.pause(1e-5)
+        
